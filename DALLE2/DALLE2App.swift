5b4579b84e7bd842c1cb0b3de9c6c4b4bd5821a3
1998code <54872601+1998code@users.noreply.github.com>
2022-10-19 12:09:47 +0800

    Initial Commit

---
+++ DALLE2/DALLE2App.swift
@@ -0,0 +1,17 @@
+//
+//  DALLE2App.swift
+//  DALLE2
+//
+//  Created by Ming on 19/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct DALLE2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
